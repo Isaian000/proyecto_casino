fa26d3d345520ed180f9e1b9303ced38dbe79cf6
Isaian000 <isaianag005@gmail.com>
2026-04-07 20:55:48 -0600

    definicion de rutas y enlazado de archivos html desde backend

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,26 @@
 import express from 'express';
+import path from 'path';
 import connectDB from './config/db';
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public'))) // rutas para los estilos e imagenes dentro de 'public/styles'
+app.use(express.static(path.join(__dirname, 'views'))) // rutas para servir archivos '.html' dentro de 'views'
 
 app.get('/', (req, res) => {
-  res.send('Servidor Casino funcionando');
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost: ${PORT}`);
-  });
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'login.html'));
+});
+
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'sigin.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost: ${PORT}`);
 });
