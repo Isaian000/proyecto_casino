53c46a121104f2c452ec942a184737b0fc661bcf
Isaian000 <isaianag005@gmail.com>
2026-04-10 20:40:37 -0600

    add func delete an account, modal and js

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { registerUser } from '../controllers/auth.controller';
-import { loginUser } from '../controllers/auth.controller';
+import { registerUser, loginUser, deleteAccount } from '../controllers/auth.controller';
 
 const router = Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.delete('/delete-account', deleteAccount);
+
 export default router;
